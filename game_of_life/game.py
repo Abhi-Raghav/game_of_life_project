b6b7075f2fa569bf4d1eeae27282155787d10134
Abhishek <abhishek@jaist.ac.jp>
2021-05-24 11:39:14 +0900

    Added some important functions.

--- game_of_life/game.py
+++ game_of_life/game.py
@@ -1,3 +1,23 @@
 import numpy as np
 
-class GameGrid:
+
+class CoreGameEngine:
+    def __init__(self, grid_size=[500, 500]):
+        self.__grid_size = np.array(grid_size, dtype=int)
+        self.__current_grid = np.zeros(self.__grid_size)
+        self.__future_grid = self.__current_grid.copy()
+        self.__iter = 0
+
+    def update_cell(self, row, col):
+        max_row, max_col = np.shape(self.__current_grid)
+        if row == max_row or col == max_col:
+            pass
+
+    def update_grid(self):
+        pass
+
+
+
+
+
+
