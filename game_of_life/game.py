1166a95f866db4661b6cba632c038d51d8bc548e
Abhishek <abhishek@jaist.ac.jp>
2021-05-28 15:48:09 +0900

    Testing of CoreGameEngine successful!

--- game_of_life/game.py
+++ game_of_life/game.py
@@ -1,12 +1,20 @@
 import numpy as np
+import os
+import time
 from rich.console import Console
-
+os.system('cls' if os.name == 'nt' else 'clear')
 
 class CoreGameEngine:
-    def __init__(self, grid_size=(10, 10)):
+    def __init__(self, grid_size=(20, 20)):
         self.__grid_size = np.array(grid_size, dtype=int)
         self.__current_grid = np.random.randint(2, size=grid_size)
-        # self.__current_grid = np.zeros(self.__grid_size)
+
+        #self.__current_grid = np.zeros(self.__grid_size)
+        #self.__current_grid[10][10] = 1
+        #self.__current_grid[10][11] = 1
+        #self.__current_grid[10][12] = 1
+
+
         self.__iter = 0
 
     def __update_cell(self, row, col):
@@ -23,7 +31,7 @@ class CoreGameEngine:
         # Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 
         max_row, max_col = np.shape(self.__current_grid)
-        if row == max_row or col == max_col:
+        if row == max_row - 1 or row == 0 or col == max_col - 1 or col == 0:
             return 0
         else:
             value_sum_neighbours = self.__current_grid[row+1][col-1] + self.__current_grid[row+1][col] + \
@@ -48,19 +56,23 @@ class CoreGameEngine:
 
     def update_grid(self):
         new_grid = self.__current_grid.copy()
-        row, col = np.shape(new_grid)
-        for i in range(row):
-            for j in range(col):
-                new_grid[i][j] = self.__update_cell(row, col)
+        max_row, max_col = np.shape(new_grid)
+        for i in range(max_row):
+            for j in range(max_col):
+                new_grid[i][j] = self.__update_cell(i, j)
+        self.__current_grid = new_grid
 
     def print_grid(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
         console = Console()
+        time.sleep(0.5)
         for row in self.__current_grid:
             for col in row:
                 if col == 1:  # alive
-                    console.print(":white_medium_square:", end=" ")
+                    console.print(":white_large_square:", end=" ")
                 else:   # dead
-                    console.print(":black_medium_square:", end=" ")
+                    # console.print(" ", end=" ")
+                    console.print(":black_large_square:", end=" ")
             console.print()
 
     def animate(self, no_of_frames=500):
@@ -69,10 +81,10 @@ class CoreGameEngine:
 
 if __name__ == "__main__":
     new_game = CoreGameEngine()
-    new_game.print_grid()
-    new_game.update_grid()
-    print()
-    new_game.print_grid()
+    for frames in range(0, 10000):
+        new_game.print_grid()
+        new_game.update_grid()
+        print()
 
 
 
